87a9cec92e355ea3b4e1a9ea025ef68ffba03d59
narinn-star <skfls2618@naver.com>
2021-04-09 22:43:36 +0900

    21.04.09

---
+++ Study/Review/Chapter03/Q11.java
@@ -0,0 +1,13 @@
+package Chapter03;
+
+public class Q11 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 0; i<args.length; i++) {
+			sum += Integer.parseInt(args[i]);
+		}
+		System.out.println((double)sum/args.length);
+	}
+
+}
--- Study/Review/Chapter03/Q13.java
+++ Study/Review/Chapter03/Q13.java
@@ -3,17 +3,27 @@ package Chapter03;
 public class Q13 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 50; i++) {
-			if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
-				if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
-					System.out.println(i + " ¹Ú¼ö Â¦Â¦");
-				}
-				else {
+		for (int i = 1; i < 100; i++) {
+			if (i < 10) {
+				if (i % 3 == 0) {
 					System.out.println(i + " ¹Ú¼ö Â¦");
 				}
+			} else {
+				if ((i / 10) % 3 == 0 && (i % 10) % 3 == 0) {
+					System.out.println(i + " ¹Ú¼ö Â¦Â¦");
+				} else {
+					if ((i / 10) % 3 == 0) {
+						System.out.println(i + " ¹Ú¼ö Â¦");
+					}
+					if ((i % 10) % 3 == 0) {
+						if (i % 10 == 0) {
+							continue;
+						} else {
+							System.out.println(i + " ¹Ú¼ö Â¦");
+						}
+					}
+				}
 			}
-			
 		}
 	}
-
 }
