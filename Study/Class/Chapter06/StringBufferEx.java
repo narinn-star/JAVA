32241cffda1b8d192f0e6c5a93d450efbf69cdb4
narinn-star <skfls2618@naver.com>
2021-06-11 21:40:07 +0900

    21.06.11

---
+++ Study/Class/Chapter06/StringBufferEx.java
@@ -0,0 +1,21 @@
+package Chapter06;
+public class StringBufferEx {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("This");
+		
+		sb.append(" is pencil"); // 문자열 덧붙이기
+		System.out.println(sb);
+		
+		sb.insert(7, " my");
+		System.out.println(sb);
+		
+		sb.replace(8,  10, "your");
+		System.out.println(sb);
+		
+		sb.delete(8,  13);
+		System.out.println(sb);
+		
+		sb.setLength(4);
+		System.out.println(sb);
+	}
+}
---
+++ Study/Class/Chapter06/ToStringEx.java
@@ -0,0 +1,20 @@
+package Chapter06;
+
+class Point1{
+	int x, y;
+	public Point1(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public String toString() {
+		return "Point1(" + x + "," + y + ")";
+	}
+}
+public class ToStringEx {
+	public static void main(String[] args) {
+		Point1 p = new Point1(2,3);
+		System.out.println(p.toString());
+		System.out.println(p); //p는 p.toString()으로 자동 변환
+		System.out.println(p + "입니다."); //p.toString() + "입니다"로 자동 변화 
+	}
+}
