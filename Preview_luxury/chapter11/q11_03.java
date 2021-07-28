410f0704f36732f18fb46834cc605753e4023f60
narinn-star <skfls2618@naver.com>
2021-07-28 22:56:13 +0900

    21.07.28

--- Preview_luxury/chapter11/q11_03.java
+++ Preview_luxury/chapter11/q11_03.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class q11_03 extends JFrame {
 	private int[] unit = {50000, 10000, 1000, 500, 100, 50, 10, 1};
-	private String[] str = {"금액", "오만원", "만원", "천원", "500원", "100원", "50원", "10원", "1원"};
+	private String[] str = {"오만원", "만원", "천원", "500원", "100원", "50원", "10원", "1원"};
 	private JLabel la = new JLabel();
 	private JTextField[] text = new JTextField[str.length];
 	private JButton btn = new JButton("계산");
@@ -18,21 +18,17 @@ public class q11_03 extends JFrame {
 		c.setLayout(new FlowLayout());
 		c.setBackground(Color.pink);
 		
-		la= new JLabel(str[0]);
-		la.setSize(40, 20);
-		la.setLocation(30, 20);
-		c.add(la);
+		c.add(new JLabel("금액"));
 		
-		text[0] = new JTextField(30);
-		text[0].setSize(50, 20);
-		text[0].setLocation(90, 20);
-		c.add(text[0]);
+		JTextField input = new JTextField(10);
+		c.add(input);
 		
 		btn.setLocation(200, 20);
+		c.add(btn);
 		btn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String s = text[0].getText();
+				String s = input.getText();
 				if(s.length() == 0)
 					return;
 				
@@ -40,30 +36,31 @@ public class q11_03 extends JFrame {
 				int res;
 				for(int i = 0; i<unit.length; i++) {
 					res = money/unit[i];
-					text[i + 1].setText(Integer.toString(res));
+					text[i].setText(Integer.toString(res));
 					if(res>0)
 						money = money % unit[i];
 				}
 			}
 		});
-		c.add(btn);
-		
-		for(int i = 1; i < str.length; i++) {
+				
+		for(int i = 0; i < str.length; i++) {
 			la = new JLabel(str[i]);
-			la.setHorizontalAlignment(JLabel.LEFT);
-			la.setSize(40, 20);
+			la.setHorizontalAlignment(JLabel.RIGHT);
+			la.setSize(50, 20);
 			la.setLocation(50, 50 + (i * 20));
 			
-			text[i] = new JTextField(30);
-			text[i].setHorizontalAlignment(JTextField.RIGHT);
+			text[i] = new JTextField(15);
+			text[i].setHorizontalAlignment(JTextField.CENTER);
 			text[i].setSize(50, 20);
 			text[i].setLocation(120, 50 + (i * 20));
 			
 			c.add(la);
 			c.add(text[i]);
 		}
+	
 		
-		setSize(300, 300);
+		
+		setSize(400, 400);
 		setVisible(true);
 	}
 
