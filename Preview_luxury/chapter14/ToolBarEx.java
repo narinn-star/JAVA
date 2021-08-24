16ae4ca43dc4a0cfddfb335dedcfa5c8f0eb4ab2
narinn-star <skfls2618@naver.com>
2021-08-24 23:47:54 +0900

    21.08.24

---
+++ Preview_luxury/chapter14/ToolBarEx.java
@@ -0,0 +1,23 @@
+package chapter14;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ToolBarEx extends JFrame {
+	private Container contentPane;
+	
+	public ToolBarEx() {
+		setTitle("툴바 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		contentPane = getContentPane();
+		setSize(400, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
