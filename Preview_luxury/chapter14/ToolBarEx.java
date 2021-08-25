fdbabbb3bd811320b155aee488332e2fa3135981
narinn-star <skfls2618@naver.com>
2021-08-25 23:02:27 +0900

    21.08.25

--- Preview_luxury/chapter14/ToolBarEx.java
+++ Preview_luxury/chapter14/ToolBarEx.java
@@ -11,13 +11,37 @@ public class ToolBarEx extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		contentPane = getContentPane();
+		createToolBar();
 		setSize(400, 200);
 		setVisible(true);
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//툴바를 생성하여 컨텐츠팬에 부착한다.
+	private void createToolBar() {
+		//툴바 생성
+		JToolBar toolBar = new JToolBar("Kitae Menu");
+		toolBar.setBackground(Color.LIGHT_GRAY);
+		
+		//툴바에 메뉴로 사용할 컴포넌트를 삽입
+		toolBar.add(new JButton("New"));
+		toolBar.add(new JButton(new ImageIcon("images/open.jpg")));
+		toolBar.addSeparator();
+		toolBar.add(new JButton(new ImageIcon("images/save.jpg")));
+		toolBar.add(new JLabel("search"));
+		toolBar.add(new JTextField("text field"));
+		
+		JComboBox<String> combo = new JComboBox<>();
+		combo.addItem("Java");
+		combo.addItem("C#");
+		combo.addItem("C");
+		combo.addItem("C++");
+		toolBar.add(combo);
+		
+		//툴파를 컨텐츠팬의 NORTH에 부착
+		contentPane.add(toolBar, BorderLayout.NORTH);
 	}
 
+	public static void main(String[] args) {
+		new ToolBarEx();
+	}
 }
